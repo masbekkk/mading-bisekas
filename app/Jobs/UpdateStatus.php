b267f82b0926a5a9257ca8ac365a5ebc5abe0bca
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-08-07 12:39:14 +0700

    init updating status from backgroud

--- app/Jobs/UpdateStatus.php
+++ app/Jobs/UpdateStatus.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Models\Mading;
+use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
@@ -23,8 +25,55 @@ class UpdateStatus implements ShouldQueue
     /**
      * Execute the job.
      */
+
     public function handle(): void
     {
-        //
+        $current = Carbon::now();
+        $madings = Mading::all();
+        foreach ($madings as $mading) {
+            $length = $mading->tanggal->diffInDays($current);
+            $status = $mading->status;
+            $newStatus = $status; // Initialize with the current status
+
+            switch ($status) {
+                case 'Tagihan DP':
+                    if ($length > 1)
+                        $newStatus = 'FPP';
+                    break;
+                case 'FPP':
+                    if ($length > 4)
+                        $newStatus = 'Pengadaan';
+                    break;
+                case 'Pengadaan':
+                    if ($length > 30)
+                        $newStatus = 'Running';
+                    break;
+                case 'Running':
+                    if ($length > 1)
+                        $newStatus = 'RETUR';
+                    break;
+                case 'RETUR':
+                    if ($length > 1)
+                        $newStatus = 'BAST';
+                    break;
+                case 'BAST':
+                    if ($length > 30)
+                        $newStatus = 'Invoice';
+                    break;
+                case 'Invoice':
+                    if ($length > 1)
+                        $newStatus = 'Lunas';
+                    break;
+                    // Uncomment and implement if needed
+                    // case 'Lunas':
+                    //     // Some logic for Lunas status
+                    //     break;
+            }
+
+            if ($newStatus !== $status) {
+                $mading->status = $newStatus;
+                $mading->save();
+            }
+        }
     }
 }
